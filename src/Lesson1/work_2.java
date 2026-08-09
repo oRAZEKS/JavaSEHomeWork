7f69554577bb5edfc2041dc9a6683caae88b4154
vladlukashov <vlad.lukashov111@ya.ru>
2026-08-09 22:48:21 +0300

    2 задание 1 урока

---
+++ src/Lesson1/work_2.java
@@ -0,0 +1,10 @@
+package Lesson1;
+
+public class work_2 {
+    static void main(String[] args) {
+        String city = "Boston";
+        int accrual = 10000;
+        double unemployed = 6.12;
+        System.out.println("строка 1" + " " +  city + " " + accrual + " " + unemployed);
+    }
+}
