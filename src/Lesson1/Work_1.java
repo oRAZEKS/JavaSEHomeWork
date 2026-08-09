baed6e5ea0027420c10a2f1f27caccb3d8db155c
vladlukashov <vlad.lukashov111@ya.ru>
2026-08-09 22:46:39 +0300

    Дз 1рвого урока

---
+++ src/Lesson1/Work_1.java
@@ -0,0 +1,8 @@
+package Lesson1;
+
+public class Work_1 {
+        static void main(String[] args) {
+            String output = "______________@@_\n______________--_\n_________/_\\__||_\n________/___\\_||_\n_______/_____\\||_\n______/|_____|\\|_\n_____/_|_____|_\\\n_______|_---_|__\n_______|_|_|_|__\n";
+            System.out.print(output);
+        }
+    }
