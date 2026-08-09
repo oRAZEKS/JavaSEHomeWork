6c7c6697074507fca61934e31417ee445f440106
vladlukashov <vlad.lukashov111@ya.ru>
2026-08-09 22:55:43 +0300

    ДЗ 4тьего урока

---
+++ src/Lesson4/Work_6.java
@@ -0,0 +1,9 @@
+package Lesson4;
+
+public class Work_6 {
+    static void main(String[] args) {
+        //Задание создать гит репозиторий и залить дз
+
+        System.out.println("Hello, WORLD!");
+    }
+}
